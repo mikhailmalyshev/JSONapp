16afaa090a888ef81dc8d03ab581f6d487c0818f
Mikhail Malyshev <malyshev090994@gmail.com>
2020-03-27 10:29:01 +0300

    Initial Commit

---
+++ JSONapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONapp
+//
+//  Created by Михаил Малышев on 27/03/2020.
+//  Copyright © 2020 Mikhail Malyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
